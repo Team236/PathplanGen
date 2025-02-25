1608b6d681d8bf22143bb3c01d295496c4eef470
sburnham3 <sburnham3@gmail.com>
2025-02-25 08:56:22 -0500

    main that runs,  output1.txt without Simulation

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 import com.pathplanner.lib.path.PathPlannerPath;
 
+import edu.wpi.first.wpilibj.RobotBase;
+
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
  * you are doing, do not modify this file except to change the parameter class to the startRobot
